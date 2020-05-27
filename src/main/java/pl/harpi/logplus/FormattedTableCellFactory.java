9e7aba1571177941829e2a7854a4e99dcd0e774d
harpipl <arkadiusz.adamczak@gmail.com>
2020-05-27 18:44:26 +0200

    Repair CellFactory types

--- src/main/java/pl/harpi/logplus/FormattedTableCellFactory.java
+++ src/main/java/pl/harpi/logplus/FormattedTableCellFactory.java
@@ -4,16 +4,16 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 
-public class FormattedTableCellFactory implements Callback<TableColumn<TableLog, String>, TableCell<TableLog, String>> {
+public class FormattedTableCellFactory<S, T> implements Callback<TableColumn<S, T>, TableCell<S, T>> {
 
     public FormattedTableCellFactory() {
     }
 
     @Override
-    public TableCell<TableLog, String> call(TableColumn<TableLog, String> p) {
-        return new TableCell<TableLog, String>() {
+    public TableCell<S, T> call(TableColumn<S, T> p) {
+        return new TableCell<S, T>() {
             @Override
-            protected void updateItem(String item, boolean empty) {
+            protected void updateItem(T item, boolean empty) {
                 getStyleClass().remove("levelInfo");
                 getStyleClass().remove("levelWarn");
                 getStyleClass().remove("levelError");
@@ -41,7 +41,7 @@ public class FormattedTableCellFactory implements Callback<TableColumn<TableLog,
 
                 getStyleClass().add(style);
 
-                setText(getItem() == null ? "" : getItem());
+                setText(getItem() == null ? "" : getItem().toString());
             }
         };
     }
